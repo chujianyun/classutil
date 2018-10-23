1beb9fa55f056b909f2cb418cf075037bfd5816e
liuwangyang <liuwangyang@corp.netease.com>
2018-10-23 14:53:36 +0800

    提供更自由的配置

--- src/main/java/com/github/chujianyun/util/ClassUtil.java
+++ src/main/java/com/github/chujianyun/util/ClassUtil.java
@@ -1,8 +1,8 @@
 package com.github.chujianyun.util;
 
+
 import com.github.chujianyun.annotation.IgnoreField;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,16 +13,36 @@ import java.util.Map;
  */
 public class ClassUtil {
 
+    private static final String SERIAL_VERSION_UID = "serialVersionUID";
 
+    /**
+     * 将类中属性（如果属性为对象则递归)，转为map键值对
+     *
+     * @param obj 带解析的对象
+     */
+    public static <T> Map<String, String> covertToNameValueMap(T obj) {
+        return covertToNameValueMap(obj, true, true);
+    }
 
 
     /**
      * 将类中属性（如果属性为对象则递归)，转为map键值对
-     * 排除带IgnoreField注解的属性 {@link IgnoreField}
-     * @param obj               带解析的对象
      *
+     * @param obj          带解析的对象
+     * @param ignoreAnnotationOn 是否开启注解   true则带有@MapIgnoreField注解的属性将被忽略 {@link IgnoreField}
      */
-    public static <T> Map<String, String> covertToNameValueMap(T obj) {
+    public static <T> Map<String, String> covertToNameValueMap(T obj, boolean ignoreAnnotationOn) {
+        return covertToNameValueMap(obj, ignoreAnnotationOn, true);
+    }
+
+    /**
+     * 将类中属性（如果属性为对象则递归)，转为map键值对
+     *
+     * @param obj                    带解析的对象
+     * @param ignoreAnnotationOn           是否开启注解   true则带有@MapIgnoreField注解的属性将被忽略 {@link IgnoreField}
+     * @param ignoreSerialVersionUID 是否忽略序列化号
+     */
+    public static <T> Map<String, String> covertToNameValueMap(T obj, boolean ignoreAnnotationOn, boolean ignoreSerialVersionUID) {
 
         if (obj == null) {
             throw new IllegalArgumentException("the object to convert is empty");
@@ -36,10 +56,15 @@ public class ClassUtil {
         Map<String, String> nameValueMap = new HashMap<>(declaredFields.length);
 
         for (Field field : declaredFields) {
-
-            if (field.isAnnotationPresent(IgnoreField.class)) {
+            // 忽略注解
+            if (ignoreAnnotationOn && field.isAnnotationPresent(IgnoreField.class)) {
                 continue;
             }
+            // 忽略序列化版本号
+            if (ignoreSerialVersionUID && field.getName().equals(SERIAL_VERSION_UID)) {
+                continue;
+            }
+
             field.setAccessible(true);
             Object value = null;
             try {
